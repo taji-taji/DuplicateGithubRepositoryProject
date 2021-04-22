61098629d224eb7118c7a83045516a58991ace07
Yutaka <y.tajika529@gmail.com>
2021-04-22 09:22:12 +0900

    modified: error handling and help

--- Swift/DuplicateGitHubProject/Sources/DuplicateGitHubProject/DuplicateGitHubProjectCommand.swift
+++ Swift/DuplicateGitHubProject/Sources/DuplicateGitHubProject/DuplicateGitHubProjectCommand.swift
@@ -7,52 +7,41 @@ struct DuplicateGitHubProjectCommand: Command {
         @Option(name: "github_access_token", short: "t", help: "GitHub Access Token")
         var githubAccessToken: String?
         
-        @Option(name: "new_project_name", short: "n")
+        @Option(name: "new_project_name", short: "n", help: "Name for duplicated project")
         var newProjectName: String?
 
-        @Option(name: "source_project_number", short: "s")
+        @Option(name: "source_project_number", short: "s", help: "Project number for source project")
         var sourceProjectNumber: Int?
 
-        @Option(name: "owner", short: "o")
+        @Option(name: "owner", short: "o", help: "Owner name for project")
         var owner: String?
 
-        @Option(name: "repository_name", short: "r")
+        @Option(name: "repository_name", short: "r", help: "Repository name for project")
         var repositoryName: String?
     }
 
     var help: String {
-        "help comment"
+        "This command duplicates a GitHub repository project, including its cards."
     }
 
     func run(using context: CommandContext, signature: Signature) throws {
-        guard let githubAccessToken = signature.githubAccessToken ?? ProcessInfo.processInfo.environment["GITHUB_TOKEN"] else {
-            // TODO: Error Handling
-            return
+        var input: Input
+        do {
+            input = try parse(signature: signature)
+        } catch let error {
+            context.console.error(error.localizedDescription)
+            exit(EXIT_FAILURE)
         }
-        guard let owner = signature.owner else {
-            // TODO: Error Handling
-            return
-        }
-        guard let newProjectName = signature.newProjectName else {
-            // TODO: Error Handling
-            return
-        }
-        guard let repositoryName = signature.repositoryName else {
-            // TODO: Error Handling
-            return
-        }
-        guard let sourceProjectNumber = signature.sourceProjectNumber else {
-            // TODO: Error Handling
-            return
-        }
-        let repository = ProjectGraphQLRepositroy(httpClient: HTTPClient(githubToken: githubAccessToken))
+        let repository = ProjectGraphQLRepositroy(httpClient: HTTPClient(githubToken: input.githubAccessToken))
         
         DispatchQueue.global().async {
             // Fetch source project
             let fetchProjectLoadingBar = context.console.loadingBar(title: "Fetching source project")
             fetchProjectLoadingBar.start()
             
-            let fetchProjectResult = repository.fetchProject(owner: owner, repo: repositoryName, projectNumber: sourceProjectNumber)
+            let fetchProjectResult = repository.fetchProject(owner: input.owner,
+                                                             repo: input.repositoryName,
+                                                             projectNumber: input.sourceProjectNumber)
             
             guard let sourceProject = try? fetchProjectResult.get() else {
                 if case let .failure(error) = fetchProjectResult {
@@ -68,7 +57,9 @@ struct DuplicateGitHubProjectCommand: Command {
             let cloneProjectLoadingBar = context.console.loadingBar(title: "Cloning project")
             cloneProjectLoadingBar.start()
             
-            let cloneProjectResult = repository.cloneProject(sourceProjectId: sourceProject.id, targetOwnerId: sourceProject.ownerId, newProjectName: newProjectName)
+            let cloneProjectResult = repository.cloneProject(sourceProjectId: sourceProject.id,
+                                                             targetOwnerId: sourceProject.ownerId,
+                                                             newProjectName: input.newProjectName)
             
             guard let newProjectNumber = try? cloneProjectResult.get() else {
                 if case let .failure(error) = fetchProjectResult {
@@ -84,7 +75,12 @@ struct DuplicateGitHubProjectCommand: Command {
             
             context.console.info("Start adding cards.")
             
-            let clonedProjectResult = fetchCloningProject(console: context.console, repository: repository, owner: owner, repo: repositoryName, projectNumber: newProjectNumber, sourceProject: sourceProject)
+            let clonedProjectResult = fetchCloningProject(console: context.console,
+                                                          repository: repository,
+                                                          owner: input.owner,
+                                                          repo: input.repositoryName,
+                                                          projectNumber: newProjectNumber,
+                                                          sourceProject: sourceProject)
             
             guard let clonedProject = try? clonedProjectResult.get() else {
                 if case let .failure(error) = clonedProjectResult {
@@ -122,6 +118,35 @@ struct DuplicateGitHubProjectCommand: Command {
 }
 
 private extension DuplicateGitHubProjectCommand {
+    struct Input {
+        let githubAccessToken: String
+        let owner: String
+        let newProjectName: String
+        let repositoryName: String
+        let sourceProjectNumber: Int
+    }
+    
+    struct ParseError: Error, LocalizedError {
+        let optionErrors: [OptionError]
+        
+        var errorDescription: String? {
+            optionErrors.map(\.localizedDescription).joined(separator: "\n")
+        }
+    }
+    
+    enum OptionError: Error, LocalizedError {
+        case missingOption(name: String)
+        
+        var errorDescription: String? {
+            switch self {
+            case .missingOption(let name):
+                return "Option \(name) is required."
+            }
+        }
+    }
+}
+
+private extension DuplicateGitHubProjectCommand {
     func fetchCloningProject(console: Console, repository: ProjectGraphQLRepositroy, owner: String, repo: String, projectNumber: Int, sourceProject: Project) -> Result<Project, Error> {
         let fetchProjectResult = repository.fetchProject(owner: owner, repo: repo, projectNumber: projectNumber)
         
@@ -135,5 +160,55 @@ private extension DuplicateGitHubProjectCommand {
         } catch let error {
             return .failure(error)
         }
+    }
+    
+    func parse(signature: Signature) throws -> Input {
+        var optionErrors: [OptionError] = []
+        
+        var githubAccessToken: String!
+        var owner: String!
+        var newProjectName: String!
+        var repositoryName: String!
+        var sourceProjectNumber: Int!
+        
+        if let _githubAccessToken = signature.githubAccessToken ?? ProcessInfo.processInfo.environment["GITHUB_TOKEN"] {
+            githubAccessToken = _githubAccessToken
+        } else {
+            optionErrors.append(.missingOption(name: signature.$githubAccessToken.name))
+        }
+        
+        if let _owner = signature.owner {
+            owner = _owner
+        } else {
+            optionErrors.append(.missingOption(name: signature.$owner.name))
+        }
+
+        if let _newProjectName = signature.newProjectName {
+            newProjectName = _newProjectName
+        } else {
+            optionErrors.append(.missingOption(name: signature.$newProjectName.name))
+        }
+        
+        if let _repositoryName = signature.repositoryName {
+            repositoryName = _repositoryName
+        } else {
+            optionErrors.append(.missingOption(name: signature.$repositoryName.name))
+        }
+        
+        if let _sourceProjectNumber = signature.sourceProjectNumber {
+            sourceProjectNumber = _sourceProjectNumber
+        } else {
+            optionErrors.append(.missingOption(name: signature.$sourceProjectNumber.name))
+        }
+        
+        guard optionErrors.isEmpty else {
+            throw ParseError(optionErrors: optionErrors)
+        }
+        
+        return Input(githubAccessToken: githubAccessToken,
+                     owner: owner,
+                     newProjectName: newProjectName,
+                     repositoryName: repositoryName,
+                     sourceProjectNumber: sourceProjectNumber)
     }
 }
